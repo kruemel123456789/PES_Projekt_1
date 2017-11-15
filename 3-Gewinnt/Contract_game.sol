cb0f159b7ac7fd66d97e419aa1777b6cdeac5b08
Chris <C>
2017-11-15 10:11:28 +0100

    Spieler hinzufügen funktioniert

--- 3-Gewinnt/Contract_game.sol
+++ 3-Gewinnt/Contract_game.sol
@@ -1,13 +1,46 @@
- pragma solidity ^0.4.0;
+pragma solidity ^0.4.18;
 
- import "get_winnings.sol";
- import "join_game.sol";
- import "place_bet.sol";
- import "unlock_tip.sol";
- import "mortal.sol";
- 
- 
- contract game is mortal
- {
- 
- }
+contract mortal {
+   /* Define variable owner of the type address */
+   address owner;
+
+   /* This function is executed at initialization and sets the owner of the contract */
+   function mortal() { owner = msg.sender; }
+
+   /* Function to recover the funds on the contract */
+   function kill() { if (msg.sender == owner) selfdestruct(owner); }
+}
+
+
+contract game is mortal
+{
+
+  //Spieler limit
+  uint256 public playerLim = 2;
+  // Spieleranzahl
+  uint256 public playerCount = 0;
+  // Einsatz + Pfand
+  uint256 public minBid = 100;
+  // Speicherung der Spieleradressen
+  address[3] public storagePlayer;
+
+   // Erfolgreich teilnahme am Spiel
+   // @param player
+   event join_success(address player);
+
+   // Es sind drei Spieler vorhande
+   //@param
+   event game_begin();
+
+   //@param valueEinsatz + Pfand
+   function join_game(uint256 value){
+     require(value > minBid && playerLim > playerCount);
+     storagePlayer[playerCount] = msg.sender; //
+     playerCount = playerCount + 1;
+     join_success(msg.sender);
+
+       if(playerLim == playerCount){
+        game_begin();
+       }
+   }
+}
--- 3-Gewinnt/join_game.sol
+++ 3-Gewinnt/join_game.sol
@@ -1,1 +1,42 @@
- 
+ pragma solidity ^0.4.18;
+
+
+ contract join_game {
+
+   //Spieler limit
+   uint public playerlim = 2;
+   // Spieleranzahl
+   uint public playercount = 0;
+   // Einsatz + Pfand
+   uint public minbid = 100;
+   // Speicherung der Spieleradressen
+   address[] public storage_player;
+
+   /**
+    * Erfolgreich teilnahme am Spiel
+    *
+    * @param player
+    */
+   event join_success(address player);
+
+   /**
+    * Es sind drei Spieler vorhanden
+    *
+    * @param
+    */
+   event game_begin();
+
+   /**
+    * @param valueEinsatz + Pfand
+    */
+   function join_game(uint value) {
+
+      require(value > minbid && playerlim > playercount )
+      storage_player[playercount] = msg.sender; //
+      playercount = playercount + 1;
+      join_success(msg.sender);
+
+      if(playerlim == playercount){
+        game_begin();
+     }
+ }
