11c4c8dfb313c3e4498a45cb6ac46f1ae4ec42ed
Chris <C>
2017-11-23 14:33:27 +0100

    modified ...

--- 3-Gewinnt/Events.js
+++ 3-Gewinnt/Events.js
@@ -1,11 +1,10 @@
 
 var browser_ballot_sol_gameContract =
-//Ersetzten
-web3.eth.contract([{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"hash","type":"bytes32"}],"name":"join_game","outputs":[{"name":"s","type":"string"}],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"tipNumber","type":"uint256"},{"name":"passphrase","type":"string"}],"name":"unlockTips","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"number","type":"uint256"},{"name":"passphrase","type":"string"},{"name":"sender","type":"address"}],"name":"greateHash","outputs":[{"name":"hash","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"checkTime","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"player","type":"address"}],"name":"join_success","type":"event"},{"anonymous":false,"inputs":[],"name":"pleaseUnlock","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"player","type":"address"}],"name":"Unlock_success","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"win","type":"uint256"}],"name":"winTheGame","type":"event"}]);
+web3.eth.contract([{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"hash","type":"bytes32"}],"name":"join_game","outputs":[{"name":"s","type":"string"}],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"tipNumber","type":"uint256"},{"name":"passphrase","type":"string"}],"name":"unlockTips","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"number","type":"uint256"},{"name":"passphrase","type":"string"},{"name":"sender","type":"address"}],"name":"greateHash","outputs":[{"name":"hash","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"checkTime","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"player","type":"address"}],"name":"join_success","type":"event"},{"anonymous":false,"inputs":[],"name":"pleaseUnlock","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"player","type":"address"}],"name":"Unlock_success","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"win","type":"uint256"}],"name":"winTheGame","type":"event"},{"anonymous":false,"inputs":[],"name":"breakGame","type":"event"},{"anonymous":false,"inputs":[],"name":"resetSuccess","type":"event"}]);
 
 
 //Addresse von dem Contract eintragen
-var game = browser_ballot_sol_gameContract.at('');
+var game = browser_ballot_sol_gameContract.at('0x8a397a9103b139f8b75af8344eaf078a02e99a2b');
 
 var event_join_success = game.join_success();
 event_join_success.watch(function(error, result)
@@ -21,7 +20,7 @@ event_join_success.watch(function(error, result)
 
 });
 
-var event_pleaseUnlock = spiel.pleaseUnlock()
+var event_pleaseUnlock = game.pleaseUnlock()
 event_pleaseUnlock.watch(function(error, result)
 {
   if(!error)
@@ -35,7 +34,7 @@ event_pleaseUnlock.watch(function(error, result)
 
 });
 
-var event_Unlock_success = spiel.Unlock_success()
+var event_Unlock_success = game.Unlock_success()
 event_Unlock_success.watch(function(error, result)
 {
   if(!error)
@@ -48,7 +47,7 @@ event_Unlock_success.watch(function(error, result)
 
 });
 
-var event_winTheGame = spiel.winTheGame()
+var event_winTheGame = game.winTheGame()
 event_winTheGame.watch(function(error, result)
 {
   if(!error)
@@ -62,7 +61,7 @@ event_winTheGame.watch(function(error, result)
 });
 
 
-var event_breakGame = spiel.breakGame()
+var event_breakGame = game.breakGame()
 event_winTheGame.watch(function(error, result)
 {
   if(!error){
@@ -75,7 +74,7 @@ event_winTheGame.watch(function(error, result)
 });
 
 
-var event_resetSuccess = spiel.resetSuccess()
+var event_resetSuccess = game.resetSuccess()
 event_winTheGame.watch(function(error, result)
 {
         if(!error)
